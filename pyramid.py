4a11a0fe1f737b66c119f97034ee39e2121480ac
patchcasey <patchcasey@gmail.com>
2019-09-04 22:20:07 -0400

    added pyramid code to pset repo

--- pyramid.py
+++ pyramid.py
@@ -1,23 +1,43 @@
 #!/usr/bin/env python3
-"""Print a pyramid to the terminal
-
-A pyramid of height 3 would look like:
-
---=--
--===-
-=====
-
-"""
+########################################################################################################################
+# Code for CSCI-e-29 Pset0 relating to the pyramid question
+# Submitted by Casey Patch on 9/4/2019
+########################################################################################################################
 
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
-
+from math import ceil
 
 def print_pyramid(rows):
-    """Print a pyramid of a given height
+    """Main start of function
 
-    :param int rows: total height
+    :param rows: int
+    :return: None
     """
-    raise NotImplementedError("Called with rows={}".format(rows))
+    # Count of number of dashes across pyramid structure
+    columns = rows + rows - 1
+    # Sequence of positions of dashes in pyramid
+    sequence = list(range(columns))
+    middle = ceil((len(sequence) - 1) / 2)
+    position = []
+
+    for i in range(0, rows):
+        # Tip of pyramid will always have one =
+        if i == 0:
+            position.append(sequence.index(middle))
+
+        # Append to list positions that should be = instead of -
+        else:
+            position.append(sequence.index(middle) - i)
+            position.append(sequence.index(middle) + i)
+
+        # Draw the pyramid, inserting = in indicated positions
+        for j in range(0, columns):
+            if j in position:
+                print("=", end="")
+            else:
+                print("-", end="")
+
+        print()
 
 
 if __name__ == "__main__":
